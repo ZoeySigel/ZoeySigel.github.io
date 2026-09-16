467a207f2cfbc7f826c3095083566d48a6d2706d
YinJacky <2696407757@qq.com>
2026-09-16 10:15:49 +0800

    fix

--- src/content/profile.ts
+++ src/content/profile.ts
@@ -39,7 +39,7 @@ export type OpenSourceProject = {
 export const USER = {
   name: "Zoey Sigel",
   handle: "@ZoeySigel",
-  role: "全栈开发/后端开发/Agent开发",
+  role: "全栈开发/Agent开发",
   location: "湖南 · 长沙",
   availability: "寻找实习机会中",
   flipSentences: [
